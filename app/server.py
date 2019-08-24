45fc621f0946c21a0096b4fddcea5de62707439f
ZhaoMengjia <840381567@qq.com>
2019-08-24 21:04:18 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,9 +10,9 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_name = 'ZYL48.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['douzi', 'luofusheng', 'shenwei', 'xiaogongye', 'xiaoxue']
 path = Path(__file__).parent
 
 app = Starlette()
