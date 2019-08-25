80b7a72b19e0c78273b591f9f50b27eaf210df27
ZhaoMengjia <840381567@qq.com>
2019-08-25 12:19:40 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -66,7 +66,8 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': names[prediction]})
+    name = names[prediction]
+    return JSONResponse({'result': str(name)})
 
 
 if __name__ == '__main__':
